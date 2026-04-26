d7ae4c0a487febf7c58090fa2f3a1dd9ae8849f1
JesseFlip <jss.flppn@gmail.com>
2026-04-26 17:43:33 -0500

    Fix: Update catch-all route syntax for Render deployment compatibility

--- server/index.js
+++ server/index.js
@@ -97,7 +97,7 @@ app.post('/api/chat', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
