012aebb6c8783bf6926f8b64e6fdcd8973ed06d1
Flip <jss.flppn@gmail.com>
2026-04-27 16:42:01 -0500

    Delete server directory

--- server/models/UserProgress.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserProgressSchema = new mongoose.Schema({
-  clerkId: { type: String, required: true, unique: true },
-  completed: { type: Map, of: Boolean, default: {} },
-  updatedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('UserProgress', UserProgressSchema);
